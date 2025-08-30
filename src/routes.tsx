96d536c715aaa94d5de6ef39bc7df2c792261c39
NaimaT15 <naimatilahun77@gmail.com>
2025-08-30 19:43:49 +0300

    routing setup with create browser router

---
+++ src/routes.tsx
@@ -0,0 +1,46 @@
+// routes.tsx
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import GamePage from "./pages/Game";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
+import AboutUsPage from "./pages/About";
+import ReportPage from "./pages/Report";
+
+function MainLayout() {
+  return (
+    <div className="font-sans antialiased text-gray-800 bg-gray-50">
+      <Navbar />
+      <Outlet />
+      <Footer />
+      <BackToTop />
+    </div>
+  );
+}
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/game",
+    element: <GamePage />,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "about",
+        element: <AboutUsPage />,
+      },
+      {
+        path: "report",
+        element: <ReportPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
